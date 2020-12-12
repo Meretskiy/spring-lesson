1cc7992fb600cbd818e854d6f953432f5d991e7f
Meretskiy Aleksandr <a-meretskiy@mail.ru>
2020-12-12 19:35:15 +0300

    add spring mvc homework

---
+++ lesson03-spring-mvc/homework-mvc/src/main/java/com/meretskiy/spring/mvc/services/ProductService.java
@@ -0,0 +1,34 @@
+package com.meretskiy.spring.mvc.services;
+
+import com.meretskiy.spring.mvc.model.Product;
+import com.meretskiy.spring.mvc.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.getAllProducts();
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public void getById(Long id) {
+        productRepository.getById(id);
+    }
+}
